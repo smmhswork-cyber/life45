e11f9d11111ec55a594832c37bede206d9aa2095
smmhswork-cyber <smmhswork@gmail.com>
2026-07-18 07:15:43 -0400

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
-import { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
